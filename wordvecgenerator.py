672c186771eeba52db69bbd0cc9815415c0d9c84
Skyorca <527515645@qq.com>
2019-10-14 20:00:44 +0800

    MODIFY

--- wordvecgenerator.py
+++ wordvecgenerator.py
@@ -1,25 +1,25 @@
 from gensim.models import word2vec, KeyedVectors
 import pandas as pd
 
+#train和test形成一个大文档训练词向量
+
 ## 训练自己的词向量，并保存。
-def trainWord2Vec(file, cat):
-    '输入是把middle/train_csv test_csv的内容复制到txt里面，一行是一个文档（句子）'
+def trainWord2Vec(file):
+    '输入要首先是把middle/train_csv test_csv的data内容分别复制到txt里面，一行是一个文档（句子）'
     sentences =  word2vec.LineSentence(file) # 读取txt文件
     model = word2vec.Word2Vec(sentences, size=256, window=5, iter=15) # 训练模型
-    model.wv.save('{}_word2vec'.format(cat))
+    model.wv.save('all_word2vec')
 
 
-def testMyWord2Vec(cat):
+def testMyWord2Vec( word):
     # 读取自己的词向量，并简单测试一下 效果。
-    inp = '{}_word2vec'.format(cat)  # 读取词向量
+    inp = 'all_word2vec'  # 读取词向量
     model = KeyedVectors.load(inp)
 
-    print('空间的词向量（256维）:',model['坚定'])
-    print('打印与空间最相近的20个词语：',model.most_similar('坚定', topn=20))
+    print('{}的词向量（256维）:'.format(word), model['{}'.format(word)])
+    print('打印与{}最相近的20个词语：'.format(word), model.most_similar('{}'.format(word), topn=20))
 
 
 if __name__ == '__main__':
-    #trainWord2Vec('./middle/train.txt','train')
-    #trainWord2Vec('./middle/test.txt','test')
-    testMyWord2Vec('train')
-    
+    trainWord2Vec('./middle/train_and_test_data.txt')
+    testMyWord2Vec('数据')
