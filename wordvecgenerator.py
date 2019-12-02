338dde41dd161e0149fb76daf10d0eb658b18544
Skyorca <527515645@qq.com>
2019-12-02 22:00:04 +0800

    256->128dim

--- wordvecgenerator.py
+++ wordvecgenerator.py
@@ -7,8 +7,8 @@ import pandas as pd
 def trainWord2Vec(file):
     '输入要首先是把middle/train_csv test_csv的data内容分别复制到txt里面，一行是一个文档（句子）'
     sentences =  word2vec.LineSentence(file) # 读取txt文件
-    model = word2vec.Word2Vec(sentences, size=256, window=5, iter=15) # 训练模型
-    model.wv.save('all_word2vec')
+    model = word2vec.Word2Vec(sentences, size=128, window=5, iter=15) # 训练模型
+    model.wv.save('all_word2vec_128')
 
 
 def testMyWord2Vec( word):
@@ -16,10 +16,10 @@ def testMyWord2Vec( word):
     inp = 'all_word2vec'  # 读取词向量
     model = KeyedVectors.load(inp)
 
-    print('{}的词向量（256维）:'.format(word), model['{}'.format(word)])
+    print('{}的词向量（128维）:'.format(word), model['{}'.format(word)])
     print('打印与{}最相近的20个词语：'.format(word), model.most_similar('{}'.format(word), topn=20))
 
 
 if __name__ == '__main__':
-    trainWord2Vec('./middle/train_and_test_data.txt')
+    #trainWord2Vec('./middle/train_and_test_data.txt')
     testMyWord2Vec('数据')
